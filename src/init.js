9ad33f106ddea75858ef54fc39f0f458083affb7
Elena Mikhaylova <elena_tuzkova@yahoo.com>
2019-03-13 18:34:14 +0300

    fix step 2

--- src/init.js
+++ src/init.js
@@ -3,30 +3,35 @@ import axios from 'axios';
 import isURL from 'validator/lib/isURL';
 import { watch } from 'melanke-watchjs';
 import _ from 'lodash';
+import $ from 'jquery';
+
+const errors = {
+  invalidURL: 'URL is invalid',
+  doubleURL: 'URL has already been added',
+  errorXML: 'URL is not RSS format',
+  notFound: 'URL is not found',
+};
 
 const renderForm = (state) => {
   const inputURL = document.getElementById('inputURL');
   const btnAddFeed = document.getElementById('btnAddFeed');
   btnAddFeed.disabled = state.formState !== 'valid';
-  inputURL.disabled = state.formState === 'wait';
+  inputURL.disabled = state.formState === 'waiting';
   inputURL.value = state.formState === 'empty' ? '' : inputURL.value;
-  inputURL.style.border = state.formState === 'invalid' ? 'thick solid red' : null;
+  if (state.formState === 'invalid') {
+    inputURL.classList.add('is-invalid');
+  } else {
+    inputURL.classList.remove('is-invalid');
+  }
 };
 
 const renderError = (state) => {
   const textURLError = document.getElementById('urlError');
-  switch (state.error) {
-    case 'invalidURL': {
-      textURLError.textContent = 'URL is invalid';
-      break;
-    }
-    case 'doubleURL': {
-      textURLError.textContent = 'URL has already been added';
-      break;
-    }
-    default: {
-      textURLError.textContent = state.error;
-    }
+  const errorText = errors[state.error];
+  if (errorText) {
+    textURLError.textContent = errorText;
+  } else {
+    textURLError.textContent = state.error;
   }
 };
 
@@ -45,8 +50,9 @@ const renderArticles = (state) => {
   const divArticles = document.getElementById('articles');
   const html = state.articles
     .map((article) => {
-      const { title, link } = article;
-      return `<li class="list-group-item"><a href="${link}">${title}</a></li>`;
+      const { title, description, link } = article;
+      return `<li class="list-group-item"><a href="${link}">${title}</a>
+        <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#articleDescriptionModal" data-description="${description}">...</button></li>`;
     })
     .join('');
   divArticles.innerHTML = `<ul class="list-group">${html}</ul>`;
@@ -55,10 +61,15 @@ const renderArticles = (state) => {
 const parseXml = (xml) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(xml, 'application/xml');
+  const parserError = doc.querySelector('parsererror');
+  if (parserError) {
+    return null;
+  }
   const items = [...doc.querySelectorAll('item')].reduce((acc, item) => {
     const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
     const link = item.querySelector('link').textContent;
-    return [...acc, { title, link }];
+    return [...acc, { title, description, link }];
   }, []);
   return {
     title: doc.querySelector('channel > title').textContent,
@@ -101,18 +112,41 @@ export default () => {
   btnAddFeed.addEventListener('click', (event) => {
     event.preventDefault();
     const link = inputURL.value;
-    state.formState = 'wait';
+    state.formState = 'waiting';
     axios.get(`https://cors-anywhere.herokuapp.com/${link}`)
       .then((response) => {
         const feed = parseXml(response.data);
-        const { title, description, items } = feed;
-        state.feeds = [...state.feeds, { title, description, link }];
-        state.articles = [...state.articles, ...items];
-        state.formState = 'empty';
+        if (!feed) {
+          state.formState = 'error';
+          state.error = 'errorXML';
+        } else {
+          const { title, description, items } = feed;
+          state.feeds = [...state.feeds, { title, description, link }];
+          state.articles = [...state.articles, ...items];
+          state.formState = 'empty';
+          state.error = null;
+        }
       })
       .catch((error) => {
-        state.error = error;
-        state.formState = 'empty';
+        state.formState = 'error';
+        if (error.response.status === 404) {
+          state.error = 'notFound';
+        } else {
+          state.error = error;
+        }
       });
+  });
+
+  $('#articleDescriptionModal').on('show.bs.modal', function (event) {
+    const description = $(event.relatedTarget).data('description');
+    $(this).find('.modal-body').text(description);
+  });
+
+  const btnArticleDesription = document.querySelectorAll('button[data-toggle="modal"]');
+  btnArticleDesription.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      $('#articleDescriptionModal').modal();
+    });
   });
 };
